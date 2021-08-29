0ee8ff3d6956cef91c90a7b1364559dd055c1f32
Yodsakorn <menming1234@outlook.com>
2021-08-29 14:28:56 +0700

    About.tsx

---
+++ src/components/About.tsx
@@ -0,0 +1,10 @@
+const About = () => {
+    return (
+        <div className='text-center space-y-3'>
+            <p className='text-2xl font-semibold'>About me</p>
+            <p className='text-xl'>630612109 YODSAKORN LIKIRUNGSON</p>
+        </div>
+    )
+}
+
+export default About
